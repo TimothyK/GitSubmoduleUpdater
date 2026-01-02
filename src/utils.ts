a2b0b282160d7a02893526ddf973ff91861d9913
Timothy Klenke <Timothy.Klenke@pscl.com>
2026-01-02 03:03:07 -0700

    Remove redundant debug logging in PROD

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import * as tl from 'azure-pipelines-task-lib/task';
 export function debugLog(message: string): void {
     tl.debug(message);
     // Also output to console for VS Code debugging
-    if (process.env.SYSTEM_DEBUG === 'true') {
+    if (process.env.VSCODE_DEBUG === 'true') {
         console.log(`[DEBUG] ${message}`);
     }
 }
